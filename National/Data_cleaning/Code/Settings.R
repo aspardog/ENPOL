5bdd0283fc74a362aebdc1adbeb08b4bf3156ef3
CristinaA-Venzor <cristinaavenzor@gmail.com>
2023-10-18 16:45:23 -0700

    Migración de Settings de Dropbox a SharePoint
    por ahora una parte del código está comentada para que el segundo archivo de source que vinculo las pruebas de hipótesis

---
+++ National/Data_cleaning/Code/Settings.R
@@ -0,0 +1,169 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Script:            ENPOL 2023 - Piloto de Hipótesis
+##
+## Author(s):         F. Marcelo Torres González  (marcelo.torresgo@gmail.com)
+##                    A. Santiago Pardo G.        (spardo@worldjusticeproject.org)
+##                    Carlos A. Toruño Paniagua   (ctoruno@worldjusticeproject.org)
+##                    Cristina Álvarez Venzor     (calvarez@worldjusticeproject.org)
+##
+## Dependencies:      World Justice Project
+##
+## Creation date:     June 7th, 2023
+##
+## This version:      June 7th, 2023
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Outline:                                                                                           ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 1.  Required packages                                                                                    ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Required packages
+
+if ('pacman' %in% rownames(installed.packages()) == FALSE) {
+  install.packages(pacman,
+                   dependencies = TRUE)
+}
+library(pacman)
+
+p_load(char = c(
+  
+  # Data Loading
+  "haven", "foreign", "openxlsx", "readxl", "writexl", "xlsx",
+  
+  # Utilities
+  "margins", "english", "quarto", "kableExtra", "sysfonts", "magrittr",
+  
+  # Good 'ol Tidyverse
+  "tidyverse"
+  
+))
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 2.  Dropbox Path                                                                                      ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# OnDrive/SharePoint path
+
+# You can find out your username by inputting Sys.info()["user"] on the R Console.
+
+if (Sys.info()["user"] == "marcelo") {
+  path2DB <- paste0("/Users/marcelo/Dropbox/ENPOL/")
+  
+} else if (Sys.info()["user"] == "santiagopardo") {
+  path2DB <- paste0("PATH SANTIAGO")
+  
+} else if (Sys.info()["user"] == "USER VERO"){
+  path2DB <- paste0("PATH VERO")
+  
+} else if (Sys.info()["user"] == "arturoluna"){
+  path2DB <- paste0("/Users/arturoluna/Dropbox/ENPOL/")
+  
+} else if (Sys.info()["user"] == "cristinaalvarez"){
+  path2DB <- paste0("/Users/cristinaalvarez/Library/CloudStorage/OneDrive-WorldJusticeProject/LOCAL/ENPOL/National")
+  
+} else{
+  path2DB <- "PLEASE INSERT YOUR PERSONAL PATH TO THE  WJP - DATA ANALYTICS DIRECTORY"
+}
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 3.  Fonts                                                                                                ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#Loading fonts
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 4.  WJP theme                                                                                            ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Defining a ggplot WJP theme
+WJP_theme <- function() {
+  theme(panel.background   = element_blank(),
+        plot.background    = element_blank(),
+        panel.grid.major   = element_line(size     = 0.25,
+                                          colour   = "#5e5c5a",
+                                          linetype = "dashed"),
+        panel.grid.minor   = element_blank(),
+        axis.title.y       = element_text(family   = "Lato Full",
+                                          face     = "plain",
+                                          size     = 3.514598*.pt,
+                                          color    = "#524F4C",
+                                          margin   = margin(0, 10, 0, 0)),
+        axis.title.x       = element_text(family   = "Lato Full",
+                                          face     = "plain",
+                                          size     = 3.514598*.pt,
+                                          color    = "#524F4C",
+                                          margin   = margin(10, 0, 0, 0)),
+        axis.text.y        = element_text(family   = "Lato Full",
+                                          face     = "plain",
+                                          size     = 3.514598*.pt,
+                                          color    = "#524F4C"),
+        axis.text.x = element_text(family = "Lato Full",
+                                   face   = "plain",
+                                   size   = 3.514598*.pt,
+                                   color  = "#524F4C"),
+        axis.ticks  = element_blank(),
+        plot.margin  = unit(c(0, 0, 0, 0), "points")
+  ) 
+}
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 5.  Color Palette                                                                                        ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+mainCOLOR      <- c("#2a2a9A")
+countryPalette <- c("#2a2a94", "#a90099", "#3273ff", "#fa4d57", "#9d61f2", "#43a9a7", "#efa700", "#2c6d4f")
+binPalette     <- c("#003b8a", "#fa4d57")
+barsPalette    <- c("#2a2a9A", "#E2E2F7")
+glinesPalette  <- c("#2a2a94", "#a90099", "#3273ff")
+rosePalette    <- c("#20204a", "#12006b", "#2e2e95", "#4e43dd", "#756ef9", "#9c94ff", "#b1a6ff",
+                    "#cfb3ff", "#e2a4ff", "#f2aadc", "#ffd7f5")
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 7.  Loading hypothesis testing function                                                                  ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#source("Settings/prueba_hip.R")
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 7.  Creating a function that will reset the Outputs directory                                            ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Defining a function to delete previous outputs and create the country directory
+#ordnung.fn <- function(targetSection){
+  
+  # Defining the section directory in the Outputs
+#  outPaths <- c(paste0(file.path(path2DB,"Outline/",targetSection,"/Tablas/")))
+  
+  # Listing previous outputs
+#  prevOutputs <- list.files(outPaths, 
+#                            include.dirs = F, 
+#                            full.names   = T, 
+#                            recursive    = T)
+  
+  # Deleting previous outputs
+#  file.remove(prevOutputs)
+  
+#}
