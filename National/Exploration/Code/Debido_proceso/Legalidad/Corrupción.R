1afa8eb3a11f95c48700a6defc228bdc448c681f
CristinaA-Venzor <85714147+CristinaAVenzor@users.noreply.github.com>
2024-02-21 21:48:45 -0800

    Update Corrupción.R

--- National/Exploration/Code/Debido_proceso/Legalidad/Corrupción.R
+++ National/Exploration/Code/Debido_proceso/Legalidad/Corrupción.R
@@ -59,6 +59,27 @@ exp_data <- function(data, group_var, seccion, nombre) {
              row.names = F)
 }
 
+
+exp_data_wide <- function(data, group_var, seccion, nombre) {
+  # Create the new data frame
+  new_data <- data %>%
+    group_by(Anio_arresto, {{ group_var }}) %>%
+    summarise(frecuencia = n()) %>%
+    ungroup() %>% 
+    pivot_wider(
+      names_from = Anio_arresto,
+      values_from = frecuencia
+    )
+  # Save the data frame to an Excel file
+  write.xlsx(as.data.frame(new_data), 
+             file      = file.path(paste0(path2SP,
+                                          "/National/Exploration/Input/Debido_proceso/legalidad", "/exp_", seccion,".xlsx"),
+                                   fsep = "/"),  
+             sheetName = paste0(nombre),
+             append    = T,
+             row.names = F)
+}
+
 exp_data_mean <- function(data, target_col, group_col,group_col_1 = NA,seccion, nombre) {
   
   # Compute the mean by the category of the grouping column
@@ -87,11 +108,36 @@ exp_data_mean <- function(data, target_col, group_col,group_col_1 = NA,seccion, 
 # 4.1 Percepción de corrupción  ---------------------------------------------------------------
 
 
-corrupcion <- c("P5_20_1", 
-                "P5_20_2",
-                "P5_20_3")
+corrupcion <- c("P3_22_1", 
+                "P4_16_1",
+                "P5_46_1")
 
-Main_database_2008 <- clean_columns.fn(Main_database_2008, presuncion) %>% 
-  rename(vidrio  =  P5_20_1,
-         esposado = P5_20_2,
-         uniforme = P5_20_3) %>% 
+Main_database_2008 <- clean_columns.fn(Main_database_2008, corrupcion) %>% 
+  rename(policia  =  P3_22_1,
+         mp = P4_16_1,
+         juez = P5_46_1) 
+
+## policía dinero a cambio de liberación
+exp_data_wide(
+  data = Main_database_2008,
+  group_var = policia,
+  seccion = "corrupcion",
+  nombre = "policia_años"
+)
+
+## mp dinero a cambio de liberación
+exp_data_wide(
+  data = Main_database_2008,
+  group_var = mp,
+  seccion = "corrupcion",
+  nombre = "mp_años"
+)
+
+## juez dinero a cambio de liberación
+exp_data_wide(
+  data = Main_database_2008,
+  group_var = juez,
+  seccion = "corrupcion",
+  nombre = "juez_años"
+)
+
