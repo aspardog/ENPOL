73c0f6392b8fb58982894683a9840f2e1338a01a
CristinaA-Venzor <85714147+CristinaAVenzor@users.noreply.github.com>
2024-03-13 18:52:43 -0700

    Update RunMe.R

--- National/Presentations/INL_MARZO/RunMe.R
+++ National/Presentations/INL_MARZO/RunMe.R
@@ -44,23 +44,200 @@ Indicators_database <- Indicators_database %>%
          indicator_general = round(indicator_general, 1)) %>% 
   filter(eliminar != 1)
 
-# 1. Histograma  ------------------------------------
-
-HistGraph <- HistGraph.fn("indicator_general", .1 , "Indicador General")
-HistGraph
-
-HistGraph <- HistGraph.fn("indicator_GDH", .1 , "Indicador Garantía de Derechos Humanos")
-HistGraph
-
-
-HistGraph <- HistGraph.fn("indicator_UAA", .1 , "Indicador Uso Arbitrario de la Fuerza")
-HistGraph
-
-HistGraph <- HistGraph.fn("indicator_PJ", .1 , "Indicador Uso Proceso Justo")
-HistGraph
-
-
 
 # Barras ------------------------------------------------------------------
 
+data2plot <- count_frequency.fn(Indicators_database$indicator_general)
 
+barChart <- BarSimpleChartViz()
+barChart
+ggsave(plot   = barChart,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Bar_indicator_general.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+
+data2plot <- count_frequency.fn(Indicators_database$indicator_GDH)
+
+barChart <- BarSimpleChartViz()
+barChart
+ggsave(plot   = barChart,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Bar_indicator_GDH.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+
+data2plot <- count_frequency.fn(Indicators_database$indicator_UAA)
+data2plot <- data2plot %>% filter(!is.na(Value))
+
+barChart <- BarSimpleChartViz()
+barChart
+ggsave(plot   = barChart,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Bar_indicator_UAA.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+data2plot <- count_frequency.fn(Indicators_database$indicator_PJ)
+data2plot <- data2plot %>% filter(!is.na(Value))
+
+barChart <- BarSimpleChartViz()
+barChart
+ggsave(plot   = barChart,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Bar_indicatorPJ.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+# Logit -----------------------------------------------------------------
+
+load(paste0(path2SP,"/National/Presentations/INL_MARZO/Input/Main_database.RData"))
+
+Main_database <- Main_database %>% 
+  mutate(eliminar = case_when( na_UAA >= 2 |
+                                 na_GDH >= 2 |
+                                 na_PJ >= 3 ~ 1,
+                               T~ 0)) %>% 
+  filter(eliminar != 1)
+
+
+# General menos del 50% de los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_general_minlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, line_size = 2)
+
+ggsave(plot   = logitPlot,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Logit_min_indicator_general.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_general_maxlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, line_size = 2)
+
+# GDH menos del 50% de los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_GDH_minlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, line_size = 2)
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Logit_min_indicator_GDH.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+# PJ menos del 50% de los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_PJ_minlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, line_size = 2)
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Logit_min_indicator_PJ.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+# UAA menos del 50% de los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_UAA_minlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, line_size = 2)
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Logit_min_indicator_UAA.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+# General todos los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_general_maxlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, line_size = 2)
+
+ggsave(plot   = logitPlot,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Logit_max_indicator_general.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+# GDH todos los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_GDH_maxlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, line_size = 2)
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Logit_max_indicator_GDH.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+# PJ todos los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_PJ_maxlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, line_size = 2)
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Logit_max_indicator_PJ.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+# UAA todos los criterios  ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_UAA_maxlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, line_size = 2)
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0( path2SP, "National/Presentations/INL_MARZO/Visualizations/Logit_max_indicator_UAA.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
