bd3255ebea3ab12a58c12167f82a8cb58b975cd1
CristinaA-Venzor <85714147+CristinaAVenzor@users.noreply.github.com>
2024-04-03 10:41:15 -0600

    Update RunMe.R

--- National/Visualization/RunMe.R
+++ National/Visualization/RunMe.R
@@ -28,17 +28,26 @@
 # loaded from the following script:
 source("Code/settings.R")
 
+# Loading functions for sections
+source("Code/S00.R")
+source("Code/S01.R")
+source("Code/S02.R")
+source("Code/S03.R")
+source("Code/S04.R")
+source("Code/S05.R")
+
+
 # Loading plotting functions from GitHub
 source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/loading.R")
 loadVIZ(set = "ENPOL")
 
 # Loading data
 load(paste0(path2SP,"/National/Data_cleaning/Output/Main_database.RData"))
-
 master_data.df <- Main_database %>% 
   filter(Anio_arresto >= as.numeric(2008)) %>% 
   filter(NSJP == 1) 
 
+# select proper filters
 # Tenemos 3 filtros y para cada visualización se decide qué filtros usar
 # sentenciado = 1
 # Anio_arresto >= 2011
@@ -75,13 +84,11 @@ master_data.df <- Main_database %>%
 ### Figure 4.2.2 cumplimiento más del 90%   de los 13 criterios -----------------------------------------------------------
 
 
-
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
 ## 2. Debido proceso                                                                                     ----
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 
 
 ### 1.1 Proceso justo -----------------------------------------------------------
