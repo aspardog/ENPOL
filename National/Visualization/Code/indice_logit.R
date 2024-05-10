0993a9ffb2c30a4e61d8d92819a0a38169a4bab6
CristinaA-Venzor <85714147+CristinaAVenzor@users.noreply.github.com>
2024-05-10 01:33:58 -0700

    Update indice_logit.R

--- National/Visualization/Code/indice_logit.R
+++ National/Visualization/Code/indice_logit.R
@@ -38,7 +38,9 @@ logit_dataBase.fn <- function(data = Main_database,
                                               "Color_piel_claro", 
                                               "LGBTQ", 
                                               "Etnia", 
-                                              "Edad_menor30"),
+                                              "Edad_menor30", 
+                                              "vulnerabilidad_economica",
+                                              "discapacidad"),
                               dependent_var
 ) {
   
@@ -67,7 +69,7 @@ logit_dataBase.fn <- function(data = Main_database,
         ),
       Etnia                 =
         case_when(
-          Etnia                 == 1 ~ "Afroamericano o indígena",
+          Etnia                 == 1 ~ "Afromexicano o indígena",
           Etnia                 == 0 ~ "No se identifica con ninguna etnia",
           T ~ NA_character_
         ),
@@ -75,6 +77,18 @@ logit_dataBase.fn <- function(data = Main_database,
         case_when(
           Edad_menor30          == 1 ~ "Menor a 30 años",
           Edad_menor30          == 0 ~ "Mayor o igual a 30 años",
+          T ~ NA_character_
+        ),
+      vulnerabilidad_economica  =
+        case_when(
+          vulnerabilidad_economica == 1 ~ "Vulnerable economicamente",
+          vulnerabilidad_economica == 0 ~ "No vulnerable economicamente",
+          T ~ NA_character_
+        ),
+      discapacidad      =
+        case_when(
+          discapacidad == 1 ~ "Reporta algún tipo de discapacidad",
+          discapacidad == 0 ~ "No presenta discapacidad",
           T ~ NA_character_
         )
     )
@@ -109,17 +123,27 @@ logit_dataBase.fn <- function(data = Main_database,
         ),
       Etnia                 =
         if_else(
-          Etnia %in% "Afroamericano o indígena",
-          "ZAfroamericano o indígena", Etnia
+          Etnia %in% "Afromexicano o indígena",
+          "ZAfromexicano o indígena", Etnia
         ),
       Edad_menor30          =
         if_else(
           Edad_menor30 %in% "Menor a 30 años",
           "ZMenor a 30 años", Edad_menor30
         ),
+      vulnerabilidad_economica          =
+        if_else(
+          vulnerabilidad_economica %in% "Vulnerable economicamente",
+          "ZVulnerable economicamente", Edad_menor30
+        ),
+      discapacidad          =
+        if_else(
+          discapacidad %in% "Reporta algún tipo de discapacidad",
+          "ZReporta algún tipo de discapacidad", discapacidad
+        ),
     ) %>%
     arrange(Sexo, Educacion_superior, Color_piel_claro, LGBTQ, Etnia, Edad_menor30, 
-            Delito_unico_categ, Estado_arresto)
+            Delito_unico_categ, Estado_arresto, vulnerabilidad_economica, discapacidad)
   
   formula <- selectables %>%
     t() %>%
@@ -146,27 +170,32 @@ logit_dataBase.fn <- function(data = Main_database,
   ) %>%
     filter(factor %in% c("SexoZFemenino", "LGBTQZPertenece a la comunidad LGBTQ", 
                          "Educacion_superiorZCuenta con título de educación universitaria", 
-                         "Color_piel_claroZColor de piel claro", "EtniaZAfroamericano o indígena",
-                         "Edad_menor30ZMenor a 30 años"))
+                         "Color_piel_claroZColor de piel claro", "EtniaZAfromexicano o indígena",
+                         "Edad_menor30ZMenor a 30 años", "vulnerabilidad_economicaZVulnerable economicamente", 
+                         "discapacidadZReporta algún tipo de discapacidad"))
   
   margEff$factor <-recode(margEff$factor,
                           "SexoZFemenino" = "Mujer",
                           "LGBTQZPertenece a la comunidad LGBTQ"                     = "Perteneciente a \ncomunidad LGBTQ",
-                          "EtniaZAfroamericano o indígena"                           = "Afroamericano/a o indígena",
+                          "EtniaZAfromexicano o indígena"                            = "Afromexicano/a o indígena",
                           "Educacion_superiorZCuenta con título de educación universitaria"  = "Con educación univeristaria \n o más",
                           "Edad_menor30ZMenor a 30 años"                             = "Menor a 30 años",
-                          "Color_piel_claroZColor de piel claro"                     = "Color de piel claro"
+                          "Color_piel_claroZColor de piel claro"                     = "Color de piel claro",
+                          "vulnerabilidad_economicaZVulnerable economicamente"       = "Vulnerable económicamente",
+                          "discapacidadZReporta algún tipo de discapacidad"          = "Persona con discapacidad"
   )
   
   data2table <- margEff %>%
     mutate(order_variable =
              case_when(
-               factor == "Mujer"                              ~ 1,
-               factor == "Perteneciente a \ncomunidad LGBTQ"  ~ 2,
-               factor == "Menor a 30 años"                    ~ 3,
+               factor == "Mujer"                                ~ 1,
+               factor == "Perteneciente a \ncomunidad LGBTQ"    ~ 2,
+               factor == "Menor a 30 años"                      ~ 3,
                factor == "Con educación univeristaria \n o más" ~ 4,
-               factor == "Afroamericano/a o indígena"         ~ 5,
-               factor == "Color de piel claro"               ~ 6
+               factor == "Afromexicano/a o indígena"            ~ 5,
+               factor == "Color de piel claro"                  ~ 6,
+               factor == "Vulnerable económicamente"            ~ 7,
+               factor == "Persona con discapacidad"             ~ 8,
              ),
            dependent_var  =
              dependent_var
@@ -176,22 +205,172 @@ logit_dataBase.fn <- function(data = Main_database,
   
 }
 
-# Applying plotting function
-data2plot <- logit_dataBase.fn(dependent_var = "indicator_general")
+# # Applying plotting function
+# data2plot <- logit_dataBase.fn(dependent_var = "indicator_GDH_maxlimit")
+# 
+# #Elegir el color rojo: "#fa4d57" o verde: "#43a9a7"
+# logitPlot <- logit_demo_panel(mainData = data2plot, shadow = T, shadow_color = "#43a9a7")
+# 
+# # See plot
+# logitPlot
+# 
+# ggsave(plot   = logitPlot,
+#        file   = paste0(path2SP,
+#                        "/National/Visualization",
+#                        "/Output/Debido proceso/Indice_logits/figure4.svg"), 
+#        width  = 175, 
+#        height = 85,
+#        units  = "mm",
+#        dpi    = 72,
+#        device = "svg")
 
-#Elegir el color rojo: "#fa4d57" o verde: "#43a9a7"
-logitPlot <- logit_demo_panel(mainData = data2plot, shadow = "#43a9a7")
 
-# See plot
-logitPlot
+# General todos los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_general_maxlimit")
+
+logitPlot <- logit_demo_panel_max(mainData = data2plot, shadow = T, shadow_color = "#43a9a7")
 
 ggsave(plot   = logitPlot,
        file   = paste0(path2SP,
                        "/National/Visualization",
-                       "/Output/Debido proceso/Uso excesivo/figure4.svg"), 
-       width  = 175, 
-       height = 85,
+                       "/Output/Debido proceso/Indice_logits/figure1.svg"), 
+       width  = 110, 
+       height = 75,
        units  = "mm",
        dpi    = 72,
        device = "svg")
+
+# General almenos del 50% de los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_general_minlimit")
+
+logitPlot <- logit_demo_panel_min(mainData = data2plot, shadow = T, shadow_color = "#fa4d57")
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0(path2SP,
+                       "/National/Visualization",
+                       "/Output/Debido proceso/Indice_logits/figure2.svg"), 
+       width  = 110, 
+       height = 75,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+# PJ todos los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_PJ_maxlimit")
+
+logitPlot <- logit_demo_panel_max(mainData = data2plot, shadow = T, shadow_color = "#43a9a7")
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0(path2SP,
+                       "/National/Visualization",
+                       "/Output/Debido proceso/Indice_logits/figure3.svg"), 
+       width  = 110, 
+       height = 75,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+
+
+# PJ menos del 50% de los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_PJ_minlimit")
+
+logitPlot <- logit_demo_panel_min(mainData = data2plot, shadow = T, shadow_color = "#fa4d57")
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0(path2SP,
+                       "/National/Visualization",
+                       "/Output/Debido proceso/Indice_logits/figure4.svg"), 
+       width  = 110, 
+       height = 75,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+# UAA todos los criterios  ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_UAA_maxlimit")
+
+logitPlot <- logit_demo_panel_max(mainData = data2plot, shadow = T, shadow_color = "#43a9a7")
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0(path2SP,
+                       "/National/Visualization",
+                       "/Output/Debido proceso/Indice_logits/figure5.svg"), 
+       width  = 110, 
+       height = 75,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+
+# UAA menos del 50% de los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_UAA_minlimit")
+
+logitPlot <- logit_demo_panel_min(mainData = data2plot, shadow = T, shadow_color = "#fa4d57")
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0(path2SP,
+                       "/National/Visualization",
+                       "/Output/Debido proceso/Indice_logits/figure6.svg"), 
+       width  = 110, 
+       height = 75,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+# GDH todos los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_GDH_maxlimit")
+
+logitPlot <- logit_demo_panel_max(mainData = data2plot, shadow = T, shadow_color = "#43a9a7")
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0(path2SP,
+                       "/National/Visualization",
+                       "/Output/Debido proceso/Indice_logits/figure7.svg"), 
+       width  = 110, 
+       height = 75,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+# GDH menos del 50% de los criterios ----------------------------------
+
+
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_GDH_minlimit")
+
+logitPlot <- logit_demo_panel_min(mainData = data2plot, shadow = T, shadow_color = "#fa4d57")
+logitPlot
+ggsave(plot   = logitPlot,
+       file   = paste0(path2SP,
+                       "/National/Visualization",
+                       "/Output/Debido proceso/Indice_logits/figure8.svg"), 
+       width  = 110, 
+       height = 75,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
+
+
+
+
 
