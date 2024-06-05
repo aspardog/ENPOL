15ece54759de74dd35e173c69c0e75fab0ddb2ee
Marcelo Torres <77344458+marcelotorresg@users.noreply.github.com>
2024-06-05 17:09:46 +0200

    First take for the third robustness test

--- National/Exploration/Code/Contraste homicidios.R
+++ National/Exploration/Code/Contraste homicidios.R
@@ -8,7 +8,7 @@
 ##
 ## Creation date:     Jun 4th, 2024
 ##
-## This version:      Jun 4th, 2024
+## This version:      Jun 5th, 2024
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -64,6 +64,27 @@ snsp_ <- snsp %>%
 
 
 
+snsp_2<- snsp %>%
+  mutate(Agosto = case_when(Anio==2021 ~ 0, T ~ Agosto),
+         Septiembre = case_when(Anio==2021 ~ 0, T ~ Septiembre),
+         Octubre = case_when(Anio==2021 ~ 0, T ~ Octubre),
+         Noviembre = case_when(Anio==2021 ~ 0, T ~ Noviembre),
+         Diciembre = case_when(Anio==2021 ~ 0, T ~ Diciembre)) %>%
+  filter(Anio>=2017 & Anio<= 2021) %>%
+  mutate(Incidencia = Enero+Febrero+Marzo+Abril+Mayo+Junio+Julio+Agosto+Septiembre+Octubre+Noviembre+Diciembre) %>%
+  mutate(Delito=case_when(`Subtipo de delito`== "Homicidio doloso" ~ "Homicidio doloso, SESNSP",
+                          `Subtipo de delito`== "Homicidio culposo" ~ "Homicidio culposo, SESNSP",
+                          T ~ "Otro")) %>% 
+  group_by(Delito,Anio) %>%
+  summarize(Incidencia = sum(Incidencia)) %>%
+  mutate(Anio=as.character(Anio),
+         value2plot =  Incidencia,
+         labels = paste0(round(value2plot,0)),
+         group_var =  "Homicidios_SNSP") %>%
+  filter(Delito!="Otro")
+
+
+
 enpol_1 <- Main_database %>% 
   filter(Anio_arresto>=2017 & Anio_arresto<= 2021 & sentenciado==1)  %>%
   select(Anio_arresto,Delito_gr_1_robos,Delito_gr_2_drogas,Delito_gr_3_del_org,Delito_gr_4_lesiones,Delito_gr_5_hom_cul,
@@ -109,6 +130,27 @@ enpol_2 <- Main_database %>%
 
 
 
+enpol_3 <- Main_database %>% 
+  filter(Anio_arresto>=2017 & Anio_arresto<= 2021 & sentenciado == 1)  %>%
+  select(Anio_arresto,Delito_gr_1_robos,Delito_gr_2_drogas,Delito_gr_3_del_org,Delito_gr_4_lesiones,Delito_gr_5_hom_cul,
+         Delito_gr_6_hom_dol,Delito_gr_7_armas,Delito_gr_8_viol_fam,Delito_gr_9_secuestro,Delito_gr_10_sexuales,
+         Delito_gr_11_extorsion,Delito_gr_12_fraude,Delito_gr_13_amenazas,Delito_gr_14_otro,Delito_gr_15_ns_nr,FAC_PER)  %>% 
+  pivot_longer(cols = c(Delito_gr_1_robos,Delito_gr_2_drogas,Delito_gr_3_del_org,Delito_gr_4_lesiones,Delito_gr_5_hom_cul,
+                       Delito_gr_6_hom_dol,Delito_gr_7_armas,Delito_gr_8_viol_fam,Delito_gr_9_secuestro,Delito_gr_10_sexuales,
+                       Delito_gr_11_extorsion,Delito_gr_12_fraude,Delito_gr_13_amenazas,Delito_gr_14_otro,Delito_gr_15_ns_nr), 
+               names_to = "Tipo_de_delito", values_to = "Sentenciados") %>%
+  mutate(Sentenciados = Sentenciados*as.numeric(FAC_PER),
+         Delito = case_when(Tipo_de_delito== "Delito_gr_6_hom_dol" ~ "Homicidio doloso, ENPOL",
+                          Tipo_de_delito== "Delito_gr_5_hom_cul" ~ "Homicidio culposo, ENPOL",
+                          T ~ "Otro")) %>%
+  group_by(Anio_arresto,Delito) %>%
+  summarize(Sentenciados = sum(Sentenciados)) %>%
+  mutate(value2plot = Sentenciados ,
+         labels = paste0(round(value2plot,0)),
+         group_var =  "Sentenciados_ENPOL") %>%
+  filter(Delito!="Otro") %>%
+  rename(Anio = Anio_arresto)
+
 
 
 ### Plots
@@ -116,6 +158,7 @@ enpol_2 <- Main_database %>%
 colors4plot <- c("#003B88", "#43a9a7", "#a90099", "#fa4d57")
 
 
+# Relativos
 
 data2plot <- bind_rows(snsp_,enpol_1)
 
@@ -214,3 +257,53 @@ ggsave(plot   = plt,
        units  = "mm",
        dpi    = 72,
        device = "svg")
+
+
+#Absolutos
+
+data2plot <- bind_rows(snsp_2,enpol_3)
+
+plt <- ggplot(data2plot, 
+              aes(x     = Anio,
+                  y     = value2plot,
+                  label = labels,
+                  group = Delito,
+                  color = Delito)) +
+  geom_point(size = 2,
+             show.legend = F) +
+  geom_line(size  = 1,
+            show.legend = F) +
+  geom_text_repel(
+    size        = 3.514598,
+    show.legend = F,
+    
+    # Additional options from ggrepel package:
+    min.segment.length = 1000,
+    seed               = 42,
+    box.padding        = 0.5,
+    direction          = "y",
+    force              = 5,
+    force_pull         = 1) +
+  scale_y_continuous(limits = c(0, 105),
+                     expand = c(0,0),
+                     breaks = seq(0,100,20),
+                     labels = paste0(seq(0,100,20), "%")) %>%
+  scale_color_manual(values = colors4plot) +
+  WJP_theme() +
+  expand_limits(y = c(0, 100))+
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "#d1cfd1"),
+        axis.title.x       = element_blank(),
+        axis.title.y       = element_blank(),
+        axis.line.x        = element_line(color    = "#d1cfd1"),
+        axis.ticks.x       = element_line(color    = "#d1cfd1",
+                                          linetype = "solid"))
+
+ggsave(plot   = plt,
+       file   = paste0("National/Exploration/Output/Contraste_homicidios_3.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
---
+++ National/Exploration/Code/Prueba robustez_sentencia.R
@@ -0,0 +1,165 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Script:            ENPOL 2023 - Piloto de Hipótesis
+##
+## Author(s):         F. Marcelo Torres González  (marcelo.torresgo@gmail.com)
+##
+## Dependencies:      World Justice Project
+##
+## Creation date:     Jun 5th, 2024
+##
+## This version:      Jun 5th, 2024
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Outline:                                                                                           ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+#### +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 0.  Settings                                                                                         ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Required Packages, Fonts, ggplot theme, color palettes and other general routines are
+# loaded from the following script:
+source("National/Data_cleaning/Code/settings.R")
+library(readxl)
+library(ggrepel)
+
+load(paste0(path2DB,"/National/Data_cleaning/Output/Main_database.RData")) 
+
+Main_database <- Main_database %>% 
+  mutate(
+    P5_4_A = as.numeric(P5_4_A),
+    tiempo_sentencia = case_when(P5_4_A == 0 ~ NA_real_,
+                                 P5_4_A >= 97 ~ NA_real_,
+                                 T ~ P5_4_A))
+
+
+# Probar que el descriptivo y la regresion sin controles dan los mismos resultados:
+
+base_prueba <- Main_database %>% filter(Anio_arresto>=2011 & Anio_arresto<= 2021)
+
+prueba <- lm(det_ninguna ~ factor(Anio_arresto), data = base_prueba)
+
+contraste <- Main_database %>% 
+  filter(Anio_arresto>=2011 & Anio_arresto<= 2021) %>%
+  group_by(Anio_arresto) %>%
+  summarize(det_ninguna = mean(det_ninguna, na.rm=T)) #el descriptivo de cada anio SI es el intercepto mas el coeficiente del anio
+
+
+# Controlar por tiempo de sentencia
+
+prueba2 <- lm(det_ninguna ~ factor(Anio_arresto) + tiempo_sentencia, data = base_prueba)
+prueba3 <- lm(det_ninguna ~ factor(Anio_arresto) + tiempo_sentencia + I(tiempo_sentencia^2), data = base_prueba)
+
+
+stargazer::stargazer(prueba,prueba2,prueba3, type="text")
+
+
+# Plot
+
+
+data2plot1 <-  as.tibble(summary(prueba)$coefficients) %>% cbind(var = row.names(summary(prueba)$coefficients), .) %>% mutate(model="base")
+data2plot2 <-  as.tibble(summary(prueba2)$coefficients) %>% cbind(var = row.names(summary(prueba2)$coefficients), .) %>% mutate(model="1")
+data2plot3 <-  as.tibble(summary(prueba3)$coefficients) %>% cbind(var = row.names(summary(prueba3)$coefficients), .) %>% mutate(model="2")
+
+base_values <- c(data2plot1$Estimate[1],data2plot2$Estimate[1],data2plot3$Estimate[1])
+
+data2plot <- bind_rows(data2plot1,data2plot2,data2plot3) %>% 
+  mutate(Anio = case_when(var=="(Intercept)" ~ "2011",
+                          var=="factor(Anio_arresto)2012" ~ "2012",
+                          var=="factor(Anio_arresto)2013" ~ "2013",
+                          var=="factor(Anio_arresto)2014" ~ "2014",
+                          var=="factor(Anio_arresto)2015" ~ "2015",
+                          var=="factor(Anio_arresto)2016" ~ "2016",
+                          var=="factor(Anio_arresto)2017" ~ "2017",
+                          var=="factor(Anio_arresto)2018" ~ "2018",
+                          var=="factor(Anio_arresto)2019" ~ "2019",
+                          var=="factor(Anio_arresto)2020" ~ "2020",
+                          var=="factor(Anio_arresto)2021" ~ "2021")) %>%
+  mutate(value2plot =  case_when(model=="base" & Anio!=2011 ~ 100*(base_values[1]+Estimate),
+                                 model=="1" & Anio!=2011 ~ 100*(base_values[2]+Estimate),
+                                 model=="2" & Anio!=2011 ~ 100*(base_values[3]+Estimate),
+                                 Anio==2011 ~ 100*Estimate),
+         labels = paste0(round(value2plot,0), "%"),
+         group_var =  model) %>%
+  filter(!is.na(Anio))
+
+Estimated_diffval1 <- filter(data2plot, Anio == 2021, model == "base") %>% select(value2plot) %>% unlist()
+Estimated_diffval2 <- filter(data2plot, Anio == 2021, model == "1") %>% select(value2plot) %>% unlist()
+Estimated_diffval3 <- filter(data2plot, Anio == 2021, model == "2") %>% select(value2plot) %>% unlist()
+Estimated_diff1 <- Estimated_diffval1 - Estimated_diffval2
+Estimated_diff2 <- Estimated_diffval1 - Estimated_diffval3
+
+data2plot_4 <- data2plot2 %>% 
+  mutate(Anio = case_when(var=="(Intercept)" ~ "2011",
+                          var=="factor(Anio_arresto)2012" ~ "2012",
+                          var=="factor(Anio_arresto)2013" ~ "2013",
+                          var=="factor(Anio_arresto)2014" ~ "2014",
+                          var=="factor(Anio_arresto)2015" ~ "2015",
+                          var=="factor(Anio_arresto)2016" ~ "2016",
+                          var=="factor(Anio_arresto)2017" ~ "2017",
+                          var=="factor(Anio_arresto)2018" ~ "2018",
+                          var=="factor(Anio_arresto)2019" ~ "2019",
+                          var=="factor(Anio_arresto)2020" ~ "2020",
+                          var=="factor(Anio_arresto)2021" ~ "2021")) %>%
+  mutate(value2plot =  case_when(Anio!=2011 ~ 100*(base_values[2]+Estimate),
+                                 Anio==2011 ~ 100*Estimate)) %>%
+  filter(!is.na(Anio)) %>%
+  mutate(value2plot = value2plot + Estimated_diff1,
+         labels = paste0(round(value2plot,0), "%"),
+         group_var =  "Estimated value")
+
+data2plot <- bind_rows(data2plot,data2plot_4)
+
+colors4plot <- c("#003B88", "#43a9a7", "#a90099", "#fa4d57")
+
+plt <- ggplot(data2plot, 
+              aes(x     = Anio,
+                  y     = value2plot,
+                  label = labels,
+                  group = group_var,
+                  color = group_var)) +
+  geom_point(size = 2,
+             show.legend = F) +
+  geom_line(size  = 1,
+            show.legend = F) +
+  geom_text_repel(
+    size        = 3.514598,
+    show.legend = F,
+    
+    # Additional options from ggrepel package:
+    min.segment.length = 1000,
+    seed               = 42,
+    box.padding        = 0.5,
+    direction          = "y",
+    force              = 5,
+    force_pull         = 1) +
+  scale_y_continuous(limits = c(0, 105),
+                     expand = c(0,0),
+                     breaks = seq(0,100,20),
+                     labels = paste0(seq(0,100,20), "%")) %>%
+  scale_color_manual(values = colors4plot) +
+  WJP_theme() +
+  expand_limits(y = c(0, 100))+
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "#d1cfd1"),
+        axis.title.x       = element_blank(),
+        axis.title.y       = element_blank(),
+        axis.line.x        = element_line(color    = "#d1cfd1"),
+        axis.ticks.x       = element_line(color    = "#d1cfd1",
+                                          linetype = "solid"))
+
+ggsave(plot   = plt,
+       file   = paste0("National/Exploration/Output/Grafica_prueba_hip_sentencia_1.svg"), 
+       width  = 175, 
+       height = 85,
+       units  = "mm",
+       dpi    = 72,
+       device = "svg")
+
+
