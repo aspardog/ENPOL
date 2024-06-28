2bdc384dba7032a2935da15b9090922f1ea61164
CristinaA-Venzor <85714147+CristinaAVenzor@users.noreply.github.com>
2024-06-28 16:33:39 -0600

    Update IndiceDP.R

--- National/Visualization/Code/Parte A/IndiceDP.R
+++ National/Visualization/Code/Parte A/IndiceDP.R
@@ -17,24 +17,52 @@
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+#NOTA: Correr desde el RUNME
+
+#source("Code/settings.R")
+
+#source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/loading.R")
+#loadVIZ(set = "ENPOL")
+
+# Loading data
+load(paste0(path2SP,
+            "/National/Data_cleaning/Output/Main_database.RData"))
+
+master_data.df <- Main_database %>% 
+  filter(Anio_arresto >= as.numeric(2008)) %>% 
+  filter(NSJP == 1)                   
+
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
 ## Indicador general barras                                             ----
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+data2plot <- index_setUp.fn(data = master_data.df,
+                            main_var ="indicator_general")
 
+
+etiquetas <- data2plot$labelx
+
+plot <- BarSimpleChartViz(shade_xminvalue = 6, shade_xmaxvalue = 10, x_labels = etiquetas); plot
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
 ## Logit positivo indicador general                                            ----
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_general_maxlimit")
+
+logitPlot <- logit_demo_panel(mainData = data2plot, shadow = T, shadow_color = "#43a9a7")
+logitPlot
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
 ## Logit negativo indicador general                                            ----
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+data2plot <- logit_dataBase.fn(dependent_var = "indicator_general_minlimit")
 
+logitPlot <- logit_demo_panel(mainData = data2plot, shadow = T, shadow_color = "#fa4d57")
+logitPlot
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
 ## Indicador general por estado                                            ----
@@ -47,15 +75,36 @@
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+data2plot <- index_setUp.fn(data = master_data.df,
+                            main_var ="indicator_PJ")
+
+etiquetas <- data2plot$labelx
+
+plot <- BarSimpleChartViz(shade_xminvalue = NA, shade_xmaxvalue = NA, x_labels = etiquetas); plot
+
+
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
 ##  Uso excesivo barras                                            ----
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+data2plot <- index_setUp.fn(data = master_data.df,
+                            main_var ="indicator_UAA")
+
+etiquetas <- data2plot$labelx
+
+plot <- BarSimpleChartViz(shade_xminvalue = NA, shade_xmaxvalue = NA, x_labels = etiquetas); plot
+
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
 ##  Tortura barras                                            ----
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+data2plot <- index_setUp.fn(data = master_data.df,
+                            main_var ="indicator_GDH")
 
+
+etiquetas <- data2plot$labelx
+
+plot <- BarSimpleChartViz(shade_xminvalue = NA, shade_xmaxvalue = NA, x_labels = etiquetas); plot
