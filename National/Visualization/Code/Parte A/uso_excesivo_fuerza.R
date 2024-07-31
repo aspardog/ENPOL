9bedf8ac9ce87fb22448d3eef621a67380b6b60b
CristinaA-Venzor <85714147+CristinaAVenzor@users.noreply.github.com>
2024-07-30 18:43:21 -0600

    Update uso_excesivo_fuerza.R

--- National/Visualization/Code/Parte A/uso_excesivo_fuerza.R
+++ National/Visualization/Code/Parte A/uso_excesivo_fuerza.R
@@ -241,11 +241,136 @@ uso_fuerza_corporacion.fn <- function(
                            savePath,"/Uso excesivo fuerza",
                            "/uso_excesivo_corporacion.svg"),
          width = 189.7883,
-         height = 105,
+         height = 85,
          units  = "mm",
          dpi    = 72,
          device = "svg")
   
   return(data2plot)
   
+}
+
+
+
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 4. Tipo de acciones realizadas por las personas al momento de su detención                                                       ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+acciones_detencion.fn <- function(
+    
+  datas.df = master_data.df
+  
+) {
+  
+  df <- data.df %>%
+    filter(Anio_arresto >= 2008,
+           NSJP == 1) %>% 
+    mutate(Acc_detencion_1 = case_when(P3_15_1 == 1 ~ 1,
+                                       P3_15_1 == 0 ~ 0,
+                                       T ~ NA_real_),
+           Acc_detencion_2 = case_when(P3_15_2 == 1 ~ 1,
+                                       P3_15_2 == 0 ~ 0,
+                                       T ~ NA_real_),
+           Acc_detencion_3 = case_when(P3_15_3 == 1 ~ 1,
+                                       P3_15_3 == 0 ~ 0,
+                                       T ~ NA_real_),
+           Acc_detencion_4 = case_when(P3_15_4 == 1 ~ 1,
+                                       P3_15_4 == 0 ~ 0,
+                                       T ~ NA_real_),
+           Acc_detencion_5 = case_when(P3_15_5 == 1 ~ 1,
+                                       P3_15_5 == 0 ~ 0,
+                                       T ~ NA_real_),
+           Acc_detencion_6 = case_when(P3_15_6 == 1 ~ 1,
+                                       P3_15_6 == 0 ~ 0,
+                                       T ~ NA_real_),
+           Acc_detencion_7 = case_when(P3_15_7 == 1 ~ 1,
+                                       P3_15_7 == 0 ~ 0,
+                                       T ~ NA_real_),
+           Acc_detencion_8 = case_when(P3_15_8 == 1 ~ 1,
+                                       P3_15_8 == 0 ~ 0,
+                                       T ~ NA_real_),
+           Acc_detencion_9 = case_when(P3_15_9 == 1 ~ 1,
+                                       P3_15_9 == 0 ~ 0,
+                                       T ~ NA_real_)) %>% 
+    select(starts_with("Acc_"))
+  
+  
+  data2plot <- df %>%
+    pivot_longer(everything(), names_to = "Column", values_to = "Percentage") %>% 
+    drop_na() %>% 
+    group_by(Column) %>% 
+    summarise(across(everything(), ~ mean(. == 1, na.rm = TRUE) * 100)) %>% 
+    rename(values = Column, 
+           value2plot = Percentage) %>% 
+    mutate(
+      labels = case_when(values == "Acc_detencion_1"     ~ "Obedeció las órdenes de la autoridad que lo detuvo", 
+                         values == "Acc_detencion_2"     ~ "Portaba alguna arma punzo cortante",
+                         values == "Acc_detencion_3"   ~ "Portaba alguna arma de fuego",
+                         values == "Acc_detencion_4"   ~ "Amenazó a alguien con el arma",
+                         values == "Acc_detencion_5"   ~ "Disparó el arma de fuego (sólo personas que llevaban arma de fuego)", 
+                         values == "Acc_detencion_6"   ~ "Manipuló algún objeto para usarlo como arma", 
+                         values == "Acc_detencion_7"   ~ "Trató de sobornar a la autoridad para evitar su detención", 
+                         values == "Acc_detencion_8"   ~ "Trató de defenderse físicamente", 
+                         values == "Acc_detencion_9"   ~ "Trató de escapar para que no lo detuvieran"),
+      figure = paste0(round(value2plot, 0), "%"),
+      labels = str_wrap(labels, width = 40),
+      order_var = rank(value2plot))
+  
+  
+  
+  colors4plot <- rep("#2a2a94", 9)
+  
+  
+  plt <- ggplot(data2plot, 
+                aes(x     = reorder(labels, order_var),
+                    y     = value2plot,
+                    label = figure,
+                    color = labels)) +
+    geom_bar(stat = "identity", fill = colors4plot, color = colors4plot,
+             show.legend = F, width = 0.9) +
+    scale_fill_manual(values = colors4plot) +
+    geom_text(aes(y    = value2plot + 10 ),
+              color    = "#4a4a49",
+              family   = "Lato Full",
+              fontface = "bold") +
+    labs(y = "% of respondents") +
+    scale_y_continuous(limits = c(0, 100),
+                       breaks = seq(0,100,20),
+                       labels = paste0(seq(0,100,20), "%"),
+                       position = "right") +
+    scale_x_discrete( ) +
+    WJP_theme() +
+    theme(legend.position="none",
+          panel.grid.major.x = element_line(colour = "#d1cfd1", 
+                                            size = 0.5),
+          panel.grid.major.y = element_blank(),
+          panel.background = element_blank(),
+          panel.grid.minor = element_blank(),
+          legend.text = element_text(family = "Lato Bold"),
+          axis.title.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          axis.title.x=element_blank(),
+          axis.text.y=element_text(family = "Lato Medium",
+                                   size = 3.514598*.pt,
+                                   color = "Black", hjust = 0),
+          legend.title = element_blank())+
+    coord_flip(); plt
+  
+  ggsave(plot = plt, 
+         filename = paste0(path2SP,
+                           "/National/Visualization",
+                           "/Output/Debido proceso/",
+                           savePath,"/Uso excesivo fuerza",
+                           "/acciones_detencion.svg"),
+         width = 189.7883,
+         height = 85,
+         units  = "mm",
+         dpi    = 72,
+         device = "svg")
+  
+  return(data2plot)
 }
