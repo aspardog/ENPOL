a6d0315735713593c788abfc65ff75eecd49a8b6
CristinaA-Venzor <cristinaavenzor@gmail.com>
2024-08-21 12:28:38 -0600

    Create entrevistas_interrogatorios.R

---
+++ National/Data_validation/politica_criminal/entrevistas_interrogatorios.R
@@ -0,0 +1,22 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Script:            ENPOL - Uso excesivo de la autoridad
+##
+## Author(s):         Santiago Pardo   (spardo@worldjusticeproject.org)
+##                    Cristina Alvarez (calvarez@worldjusticeproject.org)
+##                    Marcelo Torres   ()
+##
+## Dependencies:      World Justice Project
+##
+## Creation date:     Junio 16, 2024
+##
+## This version:      Junio 16, 2024
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##                                                ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
